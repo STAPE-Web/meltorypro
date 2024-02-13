0d5becc4f535c4463ee5a55a12e755f30cab5f29
Kirill Kirilenko <ggggfetfdfgjjh2@gmail.com>
2024-02-13 21:07:46 +0500

    update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,5 @@
-import Nav from "@/ui/Nav"
-import { navigation } from "./constants"
+// import Nav from "@/ui/Nav"
+// import { navigation } from "./constants"
 import styles from "./style.module.css"
 
 const Footer = () => {
@@ -8,14 +8,14 @@ const Footer = () => {
             <div className={styles.Container}>
                 <div className={styles.Logo}>
                     <h3>meltory</h3>
-                    <p>© 2023 MELTROY <br /> All rights reserved</p>
+                    <p>© 2024 MELTROY <br /> All rights reserved</p>
                 </div>
 
                 <div className={styles.Empty} />
 
-                {navigation.map((nav, index) => (
+                {/* {navigation.map((nav, index) => (
                     <Nav array={nav.array} title={nav.title} key={index} />
-                ))}
+                ))} */}
             </div>
         </footer>
     )
